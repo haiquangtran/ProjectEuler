fc140e7606ec9086cba452248e30762ac48ca768
haiquangtran <hai.quang.tran@gmail.com>
2016-03-21 21:46:44 +1300

    attempt at problem 3. Still have to account for the bigInt.

--- src/problems/Q3.java
+++ src/problems/Q3.java
@@ -1,6 +1,7 @@
 package problems;
 
-import java.util.HashSet;
+import java.math.BigInteger;
+
 
 /**
  * The prime factors of 13195 are 5, 7, 13 and 29.
@@ -11,9 +12,53 @@ import java.util.HashSet;
 public class Q3 {
 
 	public static void main(String[] args) {
-
+		BigInteger number = new BigInteger("600851475143");
+		printAnswer(13195);
 	}
-	
 
+	public static void printAnswer(int number) {
+		System.out.println(getLargestPrime(number));
+	}
+
+	/**
+	 * Helper method for Sieve of Eratosthenes.
+	 * Initializes an array from 2 to n with true values.
+	 * @param n
+	 * @return boolean array
+	 */
+	public static boolean[] getBooleanArray(int n) {
+		// where array index represents the number.
+		boolean[] primeNumbers = new boolean[n + 1];
+
+		for (int i = 2; i <= n; i++) {
+			primeNumbers[i] = true;
+		}
+
+		return primeNumbers;
+	}
+
+	/**
+	 * Get the largest prime number that is less than or equal to n > 2 by
+	 * using Sieve of Eratosthenes.
+	 */
+	public static int getLargestPrime(int n) {
+		boolean[] primeNumbers = getBooleanArray(n);
+		int largestPrime = 2;
+
+		for (int i = 2; i <= n; i++) {
+			if (primeNumbers[i]) {
+				// Find largest prime
+				if (n % i == 0) {
+					largestPrime = i;
+				}
+				// Mark multiples of i as non-primes
+				for (int j = i; j * i <= n; j++) {
+					primeNumbers[i * j] = false;
+				}
+			}
+		}
+
+		return largestPrime;
+	}
 
 }
