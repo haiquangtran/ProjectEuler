ae516bde00ac5ce0ddefcf2e8880f0ecf261af08
haiquangtran <hai.quang.tran@gmail.com>
2016-03-23 00:28:58 +1300

    added problem 6 in project euler. (Unsolved)

---
+++ src/problems/P006.java
@@ -0,0 +1,28 @@
+package problems;
+
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ * 12 + 22 + ... + 102 = 385
+ * 
+ * The square of the sum of the first ten natural numbers is,
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * 
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+ * 3025 - 385 = 2640.
+ * 
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * 
+ * @author Hai
+ *
+ */
+public class P006 {
+	
+	public static void main(String[] args) {
+		printAnswer();
+
+	}
+	
+	public static void printAnswer() {
+		
+	}
+}
