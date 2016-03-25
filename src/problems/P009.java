b551fc9faa5dd9a1859d4a77b308c87d57e65c61
haiquangtran <hai.quang.tran@gmail.com>
2016-03-26 12:31:04 +1300

    fixed problem 9 wording. (unsolved). Also added isPythTriplet method

--- src/problems/P009.java
+++ src/problems/P009.java
@@ -2,8 +2,8 @@ package problems;
 
 /**
  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
- * a2 + b2 = c2
- * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * 
  * Find the product abc.
@@ -18,8 +18,16 @@ public class P009 {
 	}
 
 	public static void printAnswer() {
-		
+		System.out.println(isPythTriplet(3, 4, 5));
 	}
 	
+	public static boolean isPythTriplet(int a, int b, int c) {
+		if (a < b && a < c && b < c) {
+			if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
+				return true;
+			}
+		}
+		return false;
+	}
 	
 }
