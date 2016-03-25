02234ce485718a5860c4002e50cc2782a2a55823
haiquangtran <hai.quang.tran@gmail.com>
2016-03-26 12:10:03 +1300

    added problem 9. (unsolved)

---
+++ src/problems/P009.java
@@ -0,0 +1,25 @@
+package problems;
+
+/**
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a2 + b2 = c2
+ * For example, 32 + 42 = 9 + 16 = 25 = 52.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * 
+ * Find the product abc.
+ * 
+ * @author Hai
+ *
+ */
+public class P009 {
+
+	public static void main(String[] args) {
+		printAnswer();
+	}
+
+	public static void printAnswer() {
+		
+	}
+	
+	
+}
