5f5046fcfbd79d15865055d2a3bc51d4f6eec7e4
haiquangtran <hai.quang.tran@gmail.com>
2016-03-26 13:35:58 +1300

    added problem 20 (unsolved).

---
+++ src/problems/P020.java
@@ -0,0 +1,21 @@
+package problems;
+
+/**
+ * n! means n x ( n - 1 ) x ... x 3 x 2 x 1
+ *  
+ * For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ * 
+ * Find the sum of the digits in the number 100!
+ * 
+ * @author Hai
+ *
+ */
+public class P020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
