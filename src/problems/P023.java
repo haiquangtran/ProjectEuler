34d4864fbb6267bedcf5572594fe56c57f4d874f
haiquangtran <hai.quang.tran@gmail.com>
2016-04-17 18:29:15 +1200

    completed problem 23 of project euler

--- src/problems/P023.java
+++ src/problems/P023.java
@@ -1,5 +1,7 @@
 package problems;
 
+import java.util.HashSet;
+
 /**
  * A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
  * For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
@@ -17,11 +19,83 @@ package problems;
 public class P023 {
 
 	public static void main(String[] args) {
-		printAnswer();
-	}
-	
-	public static void printAnswer() {
-		
+		int n = 28123;
+
+		printAnswer(n);
 	}
 
+	public static void printAnswer(int n) {
+		System.out.println(getSumOfNonAbundantNumbersTo(n));
+	}
+
+	public static int getSumOfNonAbundantNumbersTo(int n) {
+		HashSet<Integer> sumOfAbundantNumbers = getAllSumOfTwoAbundantNumbers(n);
+		int sum = 0;
+
+		// find sum of all numbers that cannot be written as the sum of two abundant numbers
+		for (int i = 0; i < n; i++) {
+			if (!sumOfAbundantNumbers.contains(i)) {
+				sum += i;
+			}
+		}
+
+		return sum;
+	}
+
+	public static int getSumOfDivisors(int n) { 
+		int sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
+
+	public static boolean isDeficientNumber(int n) {
+		if (getSumOfDivisors(n) < n) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isAbundantNumber(int n) {
+		return !isDeficientNumber(n) && !isPerfectNumber(n);
+	}
+
+	public static boolean isPerfectNumber(int n) {
+		if (getSumOfDivisors(n) == n) {
+			return true;
+		}
+		return false;
+	}
+
+	public static HashSet<Integer> getAllAbundantNumbers(int n) {
+		HashSet<Integer> abundantNumbers = new HashSet<Integer>();
+		
+		// find all abundant numbers
+		for (int i = 0; i <= n; i++) {
+			if (isAbundantNumber(i)) {
+				abundantNumbers.add(i);
+			}
+		}
+		return abundantNumbers;		
+	}
+	
+	public static HashSet<Integer> getAllSumOfTwoAbundantNumbers(int n) {
+		HashSet<Integer> sumOfAbundantNumbers = new HashSet<Integer>();
+		HashSet<Integer> abundantNumbers = getAllAbundantNumbers(n);
+		
+		// find all numbers that are written as the sum of two abundant numbers
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= n; j++) {
+				if (abundantNumbers.contains(i) && abundantNumbers.contains(j)) {
+					int result = i + j;
+					sumOfAbundantNumbers.add(result);
+				} 
+			}
+		}
+		return sumOfAbundantNumbers;
+	}
+	
 }
